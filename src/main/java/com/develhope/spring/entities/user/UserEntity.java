63e4e2dfd06dce4caac73afc84e4f2f3ed0cd218
Bruno Orlandi <40028887+Betalk99@users.noreply.github.com>
2024-01-23 13:08:02 +0100

    aggiunta della classe UserEntity

---
+++ src/main/java/com/develhope/spring/entities/user/UserEntity.java
@@ -0,0 +1,19 @@
+package com.develhope.spring.entities.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private String psw;
+
+
+}
