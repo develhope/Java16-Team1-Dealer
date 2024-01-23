ebcac6aaf9a6299774565daeec68c6456a356958
Bruno Orlandi <40028887+Betalk99@users.noreply.github.com>
2024-01-23 16:30:03 +0100

    aggiunta ID e colonne per db

--- src/main/java/com/develhope/spring/entities/user/AdminEntity.java
+++ src/main/java/com/develhope/spring/entities/user/AdminEntity.java
@@ -1,4 +1,14 @@
 package com.develhope.spring.entities.user;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class AdminEntity extends UserEntity{
+
 }
--- src/main/java/com/develhope/spring/entities/user/UserEntity.java
+++ src/main/java/com/develhope/spring/entities/user/UserEntity.java
@@ -1,18 +1,29 @@
 package com.develhope.spring.entities.user;
 
+import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "account")
+@SuperBuilder
 public class UserEntity {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String surname;
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column(nullable = false)
     private String psw;
 
 
