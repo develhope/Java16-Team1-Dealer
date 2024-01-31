41b9216a51857e5f85817daf73852c89749cd9f5
Bruno Orlandi <40028887+Betalk99@users.noreply.github.com>
2024-01-31 16:48:07 +0100

    aggiunta OrderRepository

---
+++ src/main/java/com/develhope/spring/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.develhope.spring.repositories;
+
+import com.develhope.spring.entities.order.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+}
