ccf68f82e6cdebfafc39e978a8e513746a8264e5
Antonio <antoniodat93@gmail.com>
2024-01-31 16:11:55 +0100

    WmsLuWLt/71-creare-controller-admin-altri-10

--- src/main/java/com/develhope/spring/controllers/AdminController.java
+++ src/main/java/com/develhope/spring/controllers/AdminController.java
@@ -51,4 +51,64 @@ public class AdminController {
     public void showOrdersQtySeller() {
 
     }
+
+    @GetMapping("/show/sellers/sales/period")
+    public void showSellerSalesInPeriodRange() {
+
+    }
+
+    @GetMapping("/show/allSales/period")
+    public void showAllSalesInPerioRange() {
+
+    }
+
+    @GetMapping("/show/vehicle/orderable")
+    public void showAvailableVehicleOrderable() {
+
+    }
+
+    @GetMapping("/show/vehicle/rfdNew")
+    public void showAvailableVehiclerfdNew() {
+
+    }
+
+    @GetMapping("/show/vehicle/rfdUsed")
+    public void showAvailableVehiclerfdUsed() {
+
+    }
+
+    @GetMapping("/show/vehicle/used")
+    public void showAvailableVehicleused() {
+
+    }
+
+    @GetMapping("/show/vehicle/notAvaiable")
+    public void showAllVehicleNotAvailable(){
+
+    }
+
+    @DeleteMapping("/delete/user/{id}")
+    public void deleteSingleUser(){
+
+    }
+
+    @PatchMapping("/update/user/{id}")
+    public void updateSingleUser(){
+
+    }
+
+    @DeleteMapping("/delete/seller/{id}")
+    public void deleteSingleSeller(){
+
+    }
+
+    @PatchMapping("/update/seller/{id}")
+    public void updateSingleSeller(){
+
+    }
+
+    @GetMapping("/show/vehicle/bestSeller/period")
+    public void showVehicleBestSellerInPeriodRange(){
+
+    }
 }
