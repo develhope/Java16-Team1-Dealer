a6eb6dcca489657756ebaca9353a1370c5093846
AB <antoniobuonanno0605@gmail.com>
2024-01-31 16:32:22 +0100

    User service upload.

--- src/main/java/com/develhope/spring/repositories/UserRepo.java
+++ src/main/java/com/develhope/spring/repositories/UserRepo.java
@@ -2,6 +2,8 @@ package com.develhope.spring.repositories;
 
 import com.develhope.spring.entities.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends JpaRepository<UserEntity, Long> {
 }
