14f456b072bbfb6a3afe87f7f50e9ef94bfd3546
Bruno Orlandi <40028887+Betalk99@users.noreply.github.com>
2024-01-31 12:30:29 +0100

    adjust  orders

--- src/main/java/com/develhope/spring/controllers/AdminController.java
+++ src/main/java/com/develhope/spring/controllers/AdminController.java
@@ -1,4 +1,19 @@
 package com.develhope.spring.controllers;
 
+import com.develhope.spring.entities.order.OrderEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/admin")
+@RestController
 public class AdminController {
+
+    @PostMapping("/create")
+    public OrderEntity createOrder() {
+        return new OrderEntity();
+    }
+
+
 }
