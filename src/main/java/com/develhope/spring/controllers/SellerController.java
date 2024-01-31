eff00935705a0fe540952f9e623e633db3541a22
AB <antoniobuonanno0605@gmail.com>
2024-01-31 16:11:24 +0100

    Seller controller routes refactor.

--- src/main/java/com/develhope/spring/controllers/SellerController.java
+++ src/main/java/com/develhope/spring/controllers/SellerController.java
@@ -15,52 +15,52 @@ import java.util.Optional;
 
 public class SellerController {
 
-    @GetMapping(path = "/getVehicleById/{id}")
+    @GetMapping(path = "/show/vehicle/by/{id}")
     public Optional<VehicleEntity> getVehicleById(@PathVariable long id) {
         return null;
     }
 
-    @PostMapping(path = "/createOrderFromVehicle")
+    @PostMapping(path = "/create/order/from/vehicle")
     public Optional<OrderEntity> createOrderFromVehicle() {
         return null;
     }
 
-    @DeleteMapping(path = "/deleteOrder")
+    @DeleteMapping(path = "/delete/order")
     public Optional<OrderEntity> deleteOrder() {
         return null;
     }
 
-    @PutMapping(path = "/editOrder")
+    @PatchMapping(path = "/update/order")
     public Optional<OrderEntity> editOrder() {
         return null;
     }
 
-    @GetMapping(path = "/checkOrder")
-    public Optional<OrderEntity> checkOrder() {
+    @GetMapping(path = "/show/order/by/{id}")
+    public Optional<OrderEntity> checkOrder(@PathVariable long id) {
         return null;
     }
 
-    @PutMapping(path = "/updateOrder")
+    @PatchMapping(path = "/update/order/by/{id}")
     public Optional<OrderEntity> updateOrder() {
         return null;
     }
 
-    @GetMapping(path = "/checkAllOrdersByStatus")
+    @GetMapping(path = "/check/all/orders/by/status")
     public List<OrderEntity> checkAllOrdersByStatus() {
         return null;
     }
 
-    @PostMapping(path = "/createRent")
+    @PostMapping(path = "/create/rent")
     public Optional<RentEntity> createRent() {
         return null;
     }
 
-    @DeleteMapping(path = "/deleteRent")
+    @DeleteMapping(path = "/delete/rent")
     public Optional<RentEntity> deleteRent() {
         return null;
     }
 
-    @PutMapping(path = "/editRent")
+    @PatchMapping(path = "/update/rent")
     public Optional<RentEntity> editRent() {
         return null;
     }
