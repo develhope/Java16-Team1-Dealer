8a8fa66d74a006bff2547bd5205eb803f9d86b9e
Bruno Orlandi <40028887+Betalk99@users.noreply.github.com>
2024-01-31 14:43:30 +0100

    aggiunta di admin controller con lÃe prime 10 rotte

--- src/main/java/com/develhope/spring/controllers/AdminController.java
+++ src/main/java/com/develhope/spring/controllers/AdminController.java
@@ -1,19 +1,54 @@
 package com.develhope.spring.controllers;
 
 import com.develhope.spring.entities.order.OrderEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/admin")
 @RestController
 public class AdminController {
 
-    @PostMapping("/create")
+    @PostMapping("/create/client/order")
     public OrderEntity createOrder() {
         return new OrderEntity();
     }
 
+    @DeleteMapping("/delete/client/order")
+    public void deleteOrder() {
+    }
 
+    @PatchMapping("/update/client/order")
+    public OrderEntity updateOrder() {
+        return new OrderEntity();
+    }
+
+    @PostMapping("/create/client/rent")
+    public void createRent() {
+    }
+
+    @DeleteMapping("/delete/client/rent")
+    public void deleteRent() {
+    }
+
+    @PatchMapping("/update/client/rent")
+    public OrderEntity updateRent() {
+        return new OrderEntity();
+    }
+
+    @PostMapping("/create/client/purchase")
+    public void createPurchase() {
+    }
+
+    @DeleteMapping("/delete/client/purchase")
+    public void deletePurchase() {
+    }
+
+    @PatchMapping("/update/client/purchase")
+    public OrderEntity updatePurchase() {
+        return new OrderEntity();
+    }
+
+    @GetMapping("/show/seller/orders/qty")
+    public void showOrdersQtySeller() {
+
+    }
 }
