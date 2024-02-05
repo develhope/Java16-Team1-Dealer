8d7b60e68bbb42382023cd95d9392eac0bb364d5
Bruno Orlandi <40028887+Betalk99@users.noreply.github.com>
2024-02-05 10:15:28 +0100

    aggiunta @Component ad IDLÃogin

--- src/main/java/com/develhope/spring/dto/IdLogin.java
+++ src/main/java/com/develhope/spring/dto/IdLogin.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Component
 public class IdLogin {
 
     private Long id;
