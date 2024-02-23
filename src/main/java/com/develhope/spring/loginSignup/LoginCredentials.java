6da1acd92439807f28956cbf36073e6765d65df0
Bruno Orlandi <40028887+Betalk99@users.noreply.github.com>
2024-02-23 10:08:47 +0100

    aggiunta documentazione API LoginCredentials

--- src/main/java/com/develhope/spring/loginSignup/LoginCredentials.java
+++ src/main/java/com/develhope/spring/loginSignup/LoginCredentials.java
@@ -1,6 +1,7 @@
 package com.develhope.spring.loginSignup;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Schema(name = "Login Credentials", description = "Login Credentials")
 public class LoginCredentials {
-
+    @Schema(description = "Email", example = "mario.rossi@gmail.com", required = true)
     private String email;
+    @Schema(description = "Password", example = "password", required = true)
     private String psw;
 }
